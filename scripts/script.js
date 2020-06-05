d7e37bb0ee2fd62a9d09e27cdcc058420d6a9259
Lygia Botelho <lygiasi@gmail.com>
2020-06-05 13:29:20 -0300

    adicao de link para cadastro de empresas e modal de mensagem enviada

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,63 @@ function cadastroAlune()
 {
     window.open("https://docs.google.com/forms/d/e/1FAIpQLSfzR0lGRrPDk-lpQ38pWE5ZQnpKAPOlx40Ls2bokIuao87U4w/viewform");
 }
+function cadastroEmpresa()
+{
+    window.open("https://www.plural.host/cadastroEmpresa.html");
+}
+function logar()
+{
+    window.open("https://plural.glideapp.io/");
+}
+
+
+class Email{
+    constructor(query){
+        const form = document.querySelector(query)
+        const name = form.querySelector('[name=name]')
+        const email = form.querySelector('[name=email]')
+        const countUrl = ''
+        const mailUrl = ''
+
+        const counterUp = () => $.ajax({
+            type: 'POST',
+            url: countUrl,
+            data: form.serialize(), // serializes the form's elements.  
+            dataType: "jsonp",
+            success: function(data)
+            {    
+               alert(data); // show response from the php script.
+            }
+        })
+        
+        form.onsubmit = (e) => {
+            e.preventDefault()
+            $("#modalSucesso").modal('show')
+            
+            $.ajax({
+                url: mailUrl,
+                method: 'POST',
+                data: {
+                    u: '', 
+                    id: '', 
+                    EMAIL: email.value, 
+                    NAME: name.value
+                },
+                dataType: 'jsonp',
+                success: function(data)
+                {    
+                   counterUp()
+                }
+            });
+        }
+    }
+}
+
+
+(function Main(){
+    const email1 = new Email('#emailForm')
+    window.data = dataEvento
+})()
 
 /*let btn = document.getElementById("cadastroAlune");
 btn.addEventListener("click", cadastroAlune);
