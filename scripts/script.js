b6b88faa98119cf43e1a400b809eea7fcab230d8
Lygia Botelho <lygiasi@gmail.com>
2020-06-05 05:30:58 -0300

    adicao de links e ajustes mobile

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+/*let btn = document.getElementById("logar");
+btn.addEventListener("click", logar);
+
+
+function logar()
+{
+    window.open("https://plural.glideapp.io/");
+}*/
+
+
+/*function cadastrarAluno()
+{
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSfzR0lGRrPDk-lpQ38pWE5ZQnpKAPOlx40Ls2bokIuao87U4w/viewform");
+}*/
